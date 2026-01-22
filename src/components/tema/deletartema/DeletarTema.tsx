d4646704b75a1b9eca37dcc14edb1cf79b4eab16
Daniel Andrade <dan.andrade313@gmail.com>
2026-01-22 08:58:15 -0300

    incluido alertas personalidados na aplicação

--- src/components/tema/deletartema/DeletarTema.tsx
+++ src/components/tema/deletartema/DeletarTema.tsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../../../contexts/AuthContext"
 import type Tema from "../../../models/Tema"
 import { buscar, deletar } from "../../../services/Service"
 import { ClipLoader } from "react-spinners";
+import { ToastAlerta } from "../../../utils/ToastAlerta"
 
 function DeletarTema() {
 
@@ -12,7 +13,7 @@ function DeletarTema() {
     const [tema, setTema] = useState<Tema>({} as Tema)
 
     const [isLoading, setIsLoading] = useState<boolean>(false)
-    
+
     const { usuario, handleLogout } = useContext(AuthContext)
     const token = usuario.token
 
@@ -34,7 +35,7 @@ function DeletarTema() {
 
     useEffect(() => {
         if (token === '') {
-            alert('Você precisa estar logado')
+            ToastAlerta('Você precisa estar logado', 'info')
             navigate('/')
         }
     }, [token])
@@ -55,13 +56,13 @@ function DeletarTema() {
                 }
             })
 
-            alert('Tema deletado com sucesso')
+            ToastAlerta('Tema deletado com sucesso', 'sucesso')
 
         } catch (error: any) {
             if (error.toString().includes('401')) {
                 handleLogout()
-            }else {
-                alert('Erro ao deletar o tema.')
+            } else {
+                ToastAlerta('Erro ao deletar o tema.', 'erro')
             }
         }
 
@@ -72,34 +73,34 @@ function DeletarTema() {
     function retornar() {
         navigate("/temas")
     }
-    
+
     return (
         <div className='container w-1/3 mx-auto'>
             <h1 className='text-4xl text-center my-4'>Deletar tema</h1>
             <p className='text-center font-semibold mb-4'>
                 Você tem certeza de que deseja apagar o tema a seguir?</p>
             <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-                <header 
+                <header
                     className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>
                     Tema
                 </header>
                 <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
                 <div className="flex">
-                    <button 
+                    <button
                         className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2'
                         onClick={retornar}>
                         Não
                     </button>
-                    <button 
+                    <button
                         className='w-full text-slate-100 bg-indigo-400 
                                    hover:bg-indigo-600 flex items-center justify-center'
-                                   onClick={deletarTema}>
+                        onClick={deletarTema}>
 
-                        { isLoading ? 
-                            <ClipLoader 
-                                color="#ffffff" 
+                        {isLoading ?
+                            <ClipLoader
+                                color="#ffffff"
                                 size={24}
-                            /> : 
+                            /> :
                             <span>Sim</span>
                         }
 
